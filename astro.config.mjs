68e94ad94ff8e0b46317b4af0b5cfd0b394cc7d0
byoussfi <byoussfi@steamulo.com>
2025-03-23 01:01:09 +0100

    feat: selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://example.com",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), icon()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes
